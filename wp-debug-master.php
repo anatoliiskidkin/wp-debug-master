6e58760deff81af2517fa11cbf6cb43ddd52cb16
Anatolii Skidkin <anatoliiskidkin@gmail.com>
2023-07-13 15:50:13 +0200

    changed plugin description

--- wp-debug-master.php
+++ wp-debug-master.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Debug Master
 Plugin URI: https://debugdrill.com/wp-debug-master
-Description: A plugin to enable WP Debug functionality and provide a debug overview.
+Description: A plugin to enable Debug functionality on WordPRess sites and provide full overview and control on the debug options inside the WP Dashboard.
 Version: 1.0.0
 Author: DebugDrill
 Author URI: https://debugdrill.com
